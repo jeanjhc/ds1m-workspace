b88b1be941938566a950954e1c0af33f4c6d0164
Jean Henrique <henrique_carvalho04@hotmail.com>
2021-01-12 11:35:06 -0300

    aprendendo java no Eclipse

--- conta/src/br/senai/sp/jandira/model/Conta.java
+++ conta/src/br/senai/sp/jandira/model/Conta.java
@@ -3,13 +3,22 @@ package br.senai.sp.jandira.model;
 public class Conta {
 
 	public String tipo;
-	public double saldo;
+	private double saldo;
 	public String numero;
 	public String titular;
 	public boolean ativa;
 	
-	public void depositar() {
-		
+	public void depositar(double valorDeposito) {
+		if (valorDeposito < 0) {
+			System.out.println("O valor deve ser maior que zero.");
+		} else {
+			
+		saldo= saldo + valorDeposito;
+		}
+	}
+	
+	public void consultarSaldo() {
+		System.out.println("Saldo da conta: " + saldo );
 	}
 	
 	public void sacar() {
@@ -22,3 +31,4 @@ public class Conta {
 	}
 	
 }
+
